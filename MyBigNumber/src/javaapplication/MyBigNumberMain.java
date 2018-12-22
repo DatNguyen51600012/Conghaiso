95cf2b3abf46d5245a25df75375b5598d806e6d5
DatNguyen51600012 <43365714+DatNguyen51600012@users.noreply.github.com>
2018-12-22 16:11:42 +0700

    Add files via upload

---
+++ MyBigNumber/src/javaapplication/MyBigNumberMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+
+
+/**
+ * Tác giả:  Nguyễn Thành Đạt.
+ * Class MyBigNumber để cộng 2 số.
+ * Version v2.0
+ * Date Modified 22/12/2018
+ */
+
+public class MyBigNumberMain {
+   public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame sum = new NewJFrame();
+        sum.setVisible(true);
+    }
+}
